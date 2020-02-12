a5945d0ee1ecd1239e0fa374512096008f72a75a
Ryan Alexander <ryandalex1@outlook.com>
2020-02-12 09:40:57 -0600

    Full UI mockup minus TARGET ZONE

--- app/src/main/java/com/team2502/scout2020/Activities/PreMatchActivity.java
+++ app/src/main/java/com/team2502/scout2020/Activities/PreMatchActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 import com.team2502.scout2020.R;
 
@@ -18,6 +19,15 @@ public class PreMatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_match);
+    }
+
+    public void backButton(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Back Button Pressed", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void frontButton(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Front Button Pressed", Toast.LENGTH_SHORT);
+        toast.show();
     }
 
 }
