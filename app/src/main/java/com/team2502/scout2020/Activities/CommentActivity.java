453af23feb723d740ce43bff503ed151c56c658e
Ryan Alexander <ryandalex1@outlook.com>
2020-03-02 23:51:02 -0600

    Stripped /n from comments

--- app/src/main/java/com/team2502/scout2020/Activities/CommentActivity.java
+++ app/src/main/java/com/team2502/scout2020/Activities/CommentActivity.java
@@ -24,7 +24,7 @@ public class CommentActivity extends AppCompatActivity {
 
     public void submit(View view){
         EditText textBox = findViewById(R.id.editText);
-        timd_in_progress += "V" + textBox.getText().toString();
+        timd_in_progress += "V" + textBox.getText().toString().replaceAll("/n", "");
         Log.e("timdComment", timd_in_progress);
 
         Intent intent = new Intent(this, QRDisplayActivity.class);
